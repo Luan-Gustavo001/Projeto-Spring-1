3f47990bc20d5ea95c2eb219eaa1a5fc83ec306e
Luan Gustavo <Luan.gustavo.melo@gmail.com>
2024-02-06 22:03:12 -0300

    start project

---
+++ curso-spring/src/test/java/com/luangustadev/curso/spring/CursoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luangustadev.curso.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
